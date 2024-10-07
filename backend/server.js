d5e354f6bbcf382d3271b80c81e4c6c75ca8626e
Dev Ghildiyal <71088793+DEV-GHILDIYAL@users.noreply.github.com>
2024-10-08 00:55:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectMongo()
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://tycs-projects-frontend.onrender.com/',
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   }));
