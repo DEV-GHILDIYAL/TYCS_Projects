5498873fb31c31b55c11886ae1633b20bded2592
Shiwans Vaishya <shiwans.vaishya@gmail.com>
2024-10-09 10:03:01 +0530

    added images

--- frontend/src/components/CardSection/CardSection.jsx
+++ frontend/src/components/CardSection/CardSection.jsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
-import img from "../../assets/images/images.png";
+// import img1 from "../../assets/images/images.png";
+import img2 from "../../assets/images/images1.png";
+import img3 from "../../assets/images/images2.png";
+import img4 from "../../assets/images/images3.png";
+import img5 from "../../assets/images/images4.png";
+import img6 from "../../assets/images/images5.png";
 import "./CardSection.css";
 
 const CardSection = ({ onViewDetail, searchTerm, searchByRollNumber }) => {
   const [projects, setProjects] = useState([]);
-
+  const images = [
+    img2,img3,img4,img5,img6
+  ]
   const token = localStorage.getItem('token');
 
   useEffect(() => {
     const fetchProject = async () => {
       try {
         const response = await fetch(`${import.meta.env.VITE_BACK_URL}`, {
-        // const response = await fetch("https://tycs-projects-backend-bnlr.onrender.com/", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`, // Assuming you're using token-based auth
+            Authorization: `Bearer ${token}`,
           },
           credentials: "include",
         });
@@ -42,13 +48,19 @@ const CardSection = ({ onViewDetail, searchTerm, searchByRollNumber }) => {
     }
   });
 
+  const randomImages = filteredProjects.map(() => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex]; // Get a random image for each project
+    // return randomIndex;
+  });
+
   return (
     <div className="card-section">
       {filteredProjects.length > 0 ? (
         filteredProjects.map((project) => (
           <Card
             key={project._id}
-            image={img}
+            image={randomImages}
             title={project.title} // Retained if you want to show it on the card
             description={project.description} // Retained if you want to show it on the card
             name={project.name}
