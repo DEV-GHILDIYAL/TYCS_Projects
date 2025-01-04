9f41861f18ff14e01c376c161670b004e9622176
Dev Ghildiyal <ghildiyaldev1325@gmail.com>
2025-01-04 09:59:01 +0530

    Server.js Fixed

--- server/server.js
+++ server/server.js
@@ -16,14 +16,14 @@ const app = express();
 // In your Express app setup
 // const cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: `${process.env.FRONT_URL}`, // Your frontend URL
   credentials: true, // Important for cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
   allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'], // Allowed headers
 }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', `${process.env.FRONT_URL}`);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-csrf-token');
   next();
