5f4a0a0d5baf51ac1be76410ce7250d0832ed3f0
Dev Ghildiyal <ghildiyaldev1325@gmail.com>
2025-02-07 00:23:05 +0530

    add profile pic in home card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ image, title, description, onViewDetail, name, project }) => {
+const Card = ({ image, title, description, onViewDetail, name, project, profileImage }) => {
   return (
     <div className="card">
       <img src={image} alt={title} className="card-image" />
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
-        <p className="card-description">{description}</p>{" "}
-        {/* This is where truncation happens */}
+        <p className="card-description">{description}</p>
         <p className="card-name">
-          ~
-          {name.length > 10 ? `${name.substring(0, 5)}...` : name.split(" ")[0]}
+          ~{name.length > 10 ? `${name.substring(0, 5)}...` : name.split(" ")[0]}
         </p>
         <button className="main-card-button" onClick={() => onViewDetail(project)}>
           View Details
         </button>
       </div>
+      {/* Profile Picture */}
+      <img src={profileImage} alt="Profile" className="profile-picture" />
     </div>
   );
 };
--- client/src/components/CardSection/CardSection.jsx
+++ client/src/components/CardSection/CardSection.jsx
@@ -94,6 +94,7 @@ const CardSection = ({
             name={project.name}
             onViewDetail={onViewDetail}
             project={project}
+            profileImage="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
           />
         ))
       ) : (
