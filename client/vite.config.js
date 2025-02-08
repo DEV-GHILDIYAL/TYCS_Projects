686a9e7102965b41c06302af8e7b01f2156d1c1d
Shiwans Vaishya <shiwans.vaishya@gmail.com>
2025-02-08 13:10:52 +0530

    .......

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [visualizer({ open: true })], // Opens a report after build
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "axios"], // Split vendors
+        },
+      },
+    },
+  },
+});
