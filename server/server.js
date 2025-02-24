3729ead89fb53642c02353c292304038141588c9
Dev Ghildiyal <ghildiyaldev1325@gmail.com>
2025-02-24 14:28:52 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -27,15 +27,15 @@ app.use(cors({
   origin: `${process.env.FRONT_URL}`, // Your frontend URL
   credentials: true, // Important for cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'], // Allowed headers
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token', 'Accept', 'x-access-token'],
 }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', `${process.env.FRONT_URL}`);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-csrf-token');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header('Access-Control-Allow-Origin', `${process.env.FRONT_URL}`);
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-csrf-token');
+//   next();
+// });
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(cookieParser());
